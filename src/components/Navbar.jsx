9a336cb808c106ddf8cde5f4eebebb50950308a8
notaravinth <aravinthravichandran2004@gmail.com>
2025-02-20 21:02:32 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import assets from '../assets/assets'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='absolute  top-0 left-0 w-full z-10'>
+      <div>
+        <img src={assets.logo} alt="" />
+        <ul>
+          <a href="#Header" className='cursor-pointer hover:text-gray-400' >Home</a>
+          <a href="#Header" className='cursor-pointer hover:text-gray-400' >About</a>
+          <a href="#Header" className='cursor-pointer hover:text-gray-400' >Projects</a>
+          <a href="#Header" className='cursor-pointer hover:text-gray-400' >testimonials</a>
+        </ul>
+        <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign Up</button>
+      </div>
       
     </div>
   )
